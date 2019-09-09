9338371f99e599071be4599f0c38c69af19ced82
adhityamurali <adhityamurali03@gmail.com>
2019-09-09 22:57:31 +0530

    new

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taskview',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
