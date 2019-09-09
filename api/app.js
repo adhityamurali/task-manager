5c9ea3d1149f0040c0be9ee0830d850131964583
adhityamurali <adhityamurali03@gmail.com>
2019-09-09 23:56:52 +0530

    db and api base setup

--- api/app.js
+++ api/app.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('heyyy')
+const {mongoose} = require('./db/mongoose');
+
+const bodyParser = require('body-parser');
+
+//load mongoose models
+const{List,Task} = require('./db/models');
+
+app.use(bodyParser.json());
+//load middle ware
+
+/* Route hanfdlers */
+
+/* list routes */
+
+
+app.get('/lists',(req,res)=>{
+    
+List.find({}).then((lists)=>{
+    res.send(lists)
 })
 
+})
+
+app.post('/lists',(req,res)=>{
+    
+let title = req.body.title
+
+let newList = new List({
+    title
+});
+newList.save().then((listDoc)=>{
+    res.send(listDoc);
+})
+
+});
+
+app.patch('/lists/:id',(req,res)=>{
+
+})
+
+app.delete('/lists/:id',(req,res)=>{
+
+})
 
 app.listen(3000,()=>{
     console.log("listening on 3000")
